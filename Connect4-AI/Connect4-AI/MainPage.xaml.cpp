b04aebec261287c83f6982e409d60e117ded3442
MikiDovlekaev <miki.dovlekaev2510@gmail.com>
2023-07-11 18:46:28 +0300

    created better GUI for program

---
+++ Connect4-AI/Connect4-AI/MainPage.xaml.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace Connect4_AI;
+
+MainPage::MainPage()
+{
+    InitializeComponent();
+    Loaded += ref new Windows::UI::Xaml::RoutedEventHandler(this, &MainPage::OnLoaded);
+}
+
+void MainPage::OnLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    // Your code for Connect4-AI logic here
+    StatusTextBlock->Text = "Connect4-AI app is working!";
+}
---
+++ Connect4-AI/Connect4-AI/MainPage.xaml.h
@@ -0,0 +1,22 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace Connect4_AI
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+		void MainPage::OnLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+	};
+}
